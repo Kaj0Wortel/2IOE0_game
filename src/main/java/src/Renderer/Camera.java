e6001bcbbbaad8977223c1a7c26e62bc28ec1373
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-10-01 10:01:31 +0200

    Revert "Merge branch 'develop' into MoveGUI"
    This reverts commit 7b66556ec0cbcb915e283bd9a2efb7a0439bc7eb, reversing
    changes made to 1d168682469579839013801a6bb40c7adfbf0969.

--- src/main/java/src/Renderer/Camera.java
+++ src/main/java/src/Renderer/Camera.java
@@ -1,9 +1,6 @@
-
 package src.Renderer;
 
-
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import src.Assets.Instance;
@@ -18,9 +15,7 @@ public class Camera {
     Matrix4f viewMatrix = new Matrix4f();
 
     int speed = 2;
-    
-    final private Lock lock = new ReentrantLock();
-    
+
     private Instance focusedOn;
     private Vector3f previousPosition;
     private boolean onPlayer;
@@ -37,20 +32,14 @@ public class Camera {
 
         calculateViewMatrix();
     }
-    
-    public void calculateViewMatrix() {
-        lock.lock();
-        try {
-            viewMatrix.identity();
-            viewMatrix.rotate((float) Math.toRadians(pitch), new Vector3f(1, 0, 0));
-            viewMatrix.rotate((float) Math.toRadians(yaw), new Vector3f(0, 1, 0));
-            viewMatrix.rotate((float) Math.toRadians(roll), new Vector3f(0, 0, 1));
-            Vector3f negPos = new Vector3f(-position.x, -position.y, -position.z);
-            viewMatrix.translate(negPos);
-            
-        } finally {
-            lock.unlock();
-        }
+
+    public void calculateViewMatrix(){
+        viewMatrix.identity();
+        viewMatrix.rotate((float) Math.toRadians(pitch), new Vector3f(1, 0, 0));
+        viewMatrix.rotate((float) Math.toRadians(yaw), new Vector3f(0, 1, 0));
+        viewMatrix.rotate((float) Math.toRadians(roll), new Vector3f(0, 0, 1));
+        Vector3f negPos = new Vector3f(-position.x, -position.y, -position.z);
+        viewMatrix.translate(negPos);
     }
     
     public Matrix4f getViewMatrix(){
@@ -59,14 +48,8 @@ public class Camera {
     }
     
     public void yaw(float amt) {
-        lock.lock();
-        try {
-            yaw += amt;
-            yaw %= 360;
-            
-        } finally {
-            lock.unlock();
-        }
+        yaw += amt;
+        yaw %= 360;
     }
 
     @Deprecated
@@ -87,14 +70,8 @@ public class Camera {
      * @param factor 
      */
     public void move(float factor) {
-        lock.lock();
-        try {
-            position.x += speed * Math.sin((float) Math.toRadians(yaw)) * factor;
-            position.z -= speed * Math.cos((float) Math.toRadians(yaw)) * factor;
-            
-        } finally {
-            lock.unlock();
-        }
+        position.x += speed * Math.sin((float) Math.toRadians(yaw)) * factor;
+        position.z -= speed * Math.cos((float) Math.toRadians(yaw)) * factor;
     }
     
     @Deprecated
@@ -112,132 +89,62 @@ public class Camera {
     }
 
     public void setPosition(Vector3f position) {
-        try {
-            this.position = new Vector3f(position);
-            
-        } finally {
-            lock.unlock();
-        }
+        this.position = new Vector3f(position);
     }
-    
-    public void setFocus(Instance instance) {
-        lock.lock();
-        try {
-            previousPosition = new Vector3f(position);
-            focusedOn = instance;
-            onPlayer = true;
-            position = new Vector3f(instance.getPosition());
-            pitch = 20;
-            
-        } finally {
-            lock.unlock();
-        }
+
+    public void setFocus(Instance instance){
+        previousPosition = new Vector3f(position);
+        focusedOn = instance;
+        onPlayer = true;
+        position = new Vector3f(instance.getPosition());
+        pitch = 20;
     }
 
     public void removeFocus() {
-        lock.lock();
-        try {
-            focusedOn = null;
-            onPlayer = false;
-            position = new Vector3f(previousPosition);
-            this.yaw = 0;
-            this.pitch = 0;
-            this.roll = 0;
-            
-        } finally {
-            lock.unlock();
-        }
+        focusedOn = null;
+        onPlayer = false;
+        position = new Vector3f(previousPosition);
+        this.yaw = 0;
+        this.pitch = 0;
+        this.roll = 0;
     }
 
     public boolean isOnPlayer() {
-        lock.lock();
-        try {
-            return onPlayer;
-            
-        } finally {
-            lock.unlock();
-        }
+        return onPlayer;
     }
-    
-    public void moveToInstance() {
-        lock.lock();
-        try {
-            distanceToAsset -= 0.1f;
-            
-        } finally {
-            lock.unlock();
-        }
+
+    public void calculateInstanceValues(){
+        float angle = focusedOn.getRoty() + angleAroundAsset;
+        float horDistance = (float) (distanceToAsset * Math.cos(Math.toRadians(pitch)));
+        float verDistance = (float) (distanceToAsset * Math.sin(Math.toRadians(pitch)));
+        float x = (float) (horDistance * Math.sin(Math.toRadians(angle)));
+        float z = (float) (horDistance * Math.cos(Math.toRadians(angle)));
+        position.x = focusedOn.getPosition().x + x;
+        position.y = focusedOn.getPosition().y + verDistance;
+        position.z = focusedOn.getPosition().z + z;
+        this.yaw = - (focusedOn.getRoty() + angleAroundAsset);
+        this.yaw %= 360;
+
     }
-    
-    public void moveAwayFromInstance() {
-        lock.lock();
-        try {
-            distanceToAsset += 0.1f;
-            
-        } finally {
-            lock.unlock();
-        }
+
+    public Matrix4f getInverseViewMatrix(){
+        return new Matrix4f(viewMatrix).invertAffine();
     }
-    
-    public void rotateAroundAssetR() {
-        lock.lock();
-        try {
-            angleAroundAsset += 0.1f;
-            
-        } finally {
-            lock.unlock();
-        }
+
+    public Matrix3f getRotationMatrix(){
+        Matrix3f rotationMatrix = new Matrix3f();
+        rotationMatrix.rotate(-(float) Math.toRadians(pitch), new Vector3f(1, 0, 0));
+        rotationMatrix.rotate(-(float) Math.toRadians(yaw), new Vector3f(0, 1, 0));
+        rotationMatrix.rotate(-(float) Math.toRadians(roll), new Vector3f(0, 0, 1));
+
+        return rotationMatrix;
     }
-    
-    public void rotateAroundAssetL() {
-        lock.lock();
-        try {
-            angleAroundAsset -= 0.1f;
-            
-        } finally {
-            lock.unlock();
-        }
+
+    public float getPitch() {
+        return pitch;
     }
-    
-    public void movePitchDown() {
-        lock.lock();
-        try {
-            pitch -= 0.1f;
-            
-        } finally {
-            lock.unlock();
-        }
+
+    public float getYaw() {
+        return yaw;
     }
-    
-    public void movePitchUp() {
-        lock.lock();
-        try {
-            pitch += 0.1f;
-            
-        } finally {
-            lock.unlock();
-        }
-    }
-    
-    public void calculateInstanceValues() {
-        lock.lock();
-        try {
-            State state = focusedOn.getState();
-            float angle = state.roty + angleAroundAsset;
-            float horDistance = (float) (distanceToAsset * Math.cos(Math.toRadians(pitch)));
-            float verDistance = (float) (distanceToAsset * Math.sin(Math.toRadians(pitch)));
-            float x = (float) (horDistance * Math.sin(Math.toRadians(angle)));
-            float z = (float) (horDistance * Math.cos(Math.toRadians(angle)));
-            position.x = state.box.pos().x + x;
-            position.y = state.box.pos().y + verDistance;
-            position.z = state.box.pos().z + z;
-            this.yaw = - (state.roty + angleAroundAsset);
-            this.yaw %= 360;
-            
-        } finally {
-            lock.unlock();
-        }
-    }
-    
-    
 }
