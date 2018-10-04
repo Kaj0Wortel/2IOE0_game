727eeb5c3b06f59e382c5d7ca1157b6d22bfc154
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-10-04 15:54:56 +0200

    added stuff

--- src/main/java/src/AI/AINN.java
+++ src/main/java/src/AI/AINN.java
@@ -8,10 +8,22 @@ package src.AI;
 // Java imports
 
 
+// dl4j
+import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.optimize.api.BaseTrainingListener;
+import src.Physics.PStructAction;
+
+
 /**
  * 
  */
 public class AINN {
+    
+    private PStructAction curState = new PStructAction(0, 0, 0, 1);
+    private PStructAction newState = null;
+    private boolean updatedLeftRight = false;
+    private boolean updatedForwardBack = false;
+    
     
     public AINN() {
         // TODO init here
@@ -19,10 +31,26 @@ public class AINN {
     
     
     public void execute() {
+        updatedLeftRight = false;
+        updatedForwardBack = true;
         // TODO
     }
     
-    
+    private BaseTrainingListener listener = new BaseTrainingListener() {
+        
+        @Override
+        public void onEpochEnd(Model model) {
+            if (1 == 1) { // left-right model.
+                updatedLeftRight = true;
+                
+                
+                
+            } else { // forward-backward model.
+                updatedForwardBack = true;
+                
+            }
+        }
+    };
     
     
 }
