8d2610884cdf5f8356b3a778eff602141bc69452
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-10-04 15:21:59 +0200

    added ai classes

---
+++ src/main/java/src/AI/AINN.java
@@ -0,0 +1,28 @@
+
+package src.AI;
+
+
+// Own imports
+
+
+// Java imports
+
+
+/**
+ * 
+ */
+public class AINN {
+    
+    public AINN() {
+        // TODO init here
+    }
+    
+    
+    public void execute() {
+        // TODO
+    }
+    
+    
+    
+    
+}
