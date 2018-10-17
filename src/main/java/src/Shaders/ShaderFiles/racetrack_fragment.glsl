cba046c362b6a1cf8e925b14c509e76a249167dd
TKafoe <kafoe.tom@gmail.com>
2018-10-17 12:04:56 +0200

    Small change to shadows

--- src/main/java/src/Shaders/ShaderFiles/racetrack_fragment.glsl
+++ src/main/java/src/Shaders/ShaderFiles/racetrack_fragment.glsl
@@ -20,11 +20,6 @@ uniform mat4 modelMatrix;
 
 const int pcfPixels = 3;
 const float totalPcfPixels = pow((pcfPixels * 2 + 1),2);
-const vec2 poisson[4] = vec2[](
-                vec2( -0.94201624, -0.39906216 ),
-                vec2( 0.94558609, -0.76890725 ),
-                vec2( -0.094184101, -0.92938870 ),
-                vec2( 0.34495938, 0.29387760 ));
 
 void main() {
 
@@ -42,10 +37,10 @@ void main() {
         }
     }
 
-    float offset = 10f;
-    visible /= (totalPcfPixels + offset);
 
-    float inShadow = 1.0 - (shadowTextureCoords.w*visible);
+    visible /= totalPcfPixels;
+
+    float inShadow = 1.0 - (shadowTextureCoords.w*visible*0.6f);
 
     vec2 tex = texPass;
     tex.x *= 10;
