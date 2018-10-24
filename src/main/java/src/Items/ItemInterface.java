5ef0bb4d22e0cdd3b880a710f83efa578eb3b2c6
Barenholz D <d.barenholz@student.tue.nl>
2018-10-24 14:29:57 +0200

    New Branch

---
+++ src/main/java/src/Items/ItemInterface.java
@@ -0,0 +1,9 @@
+package src.Items;
+
+import src.Assets.instance.Instance;
+import src.Physics.PStructAction;
+import src.Physics.Physics;
+
+public interface ItemInterface {
+    void activate(Instance instance, PStructAction pStruct, Physics.ModPhysicsContext pc, Physics.ModState s);
+}
